5616416c32f3cd63fef789908264f70310b8bcf3
Leesa Ward <leesa.ward2@rmit.edu.au>
2021-09-22 17:44:28 +1000

    Add current page indicator in the header nav

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,4 @@
+
 <header class="site-header">
 	<div class="container">
 		<div class="row">
@@ -14,9 +15,29 @@
 					</button>
 				</form>
 				<ul class="menu">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="about.php">About</a></li>
-					<li><a href="contact.php">Contact</a></li>
+					<?php
+					// get the path after the domain using the $_SERVER global - may be different for local vs Jupiter
+					$currentPath = $_SERVER['PHP_SELF'];
+					// replace the part of the path we don't want with an empty string so we end up with just the .php filename we're linking to
+					$filename = str_replace('/webdemo02/', '', $currentPath);
+					?>
+					<?php if($filename == 'index.php') { ?>
+						<li><a class="current-page" href="index.php">Home</a></li>
+					<?php } else { ?>
+						<li><a href="index.php">Home</a></li>
+					<?php } ?>
+
+					<?php if($filename == 'about.php') { ?>
+						<li><a class="current-page" href="about.php">About</a></li>
+					<?php } else { ?>
+						<li><a href="about.php">About</a></li>
+					<?php } ?>
+
+					<?php if($filename == 'contact.php') { ?>
+						<li><a class="current-page" href="contact.php">Contact</a></li>
+					<?php } else { ?>
+						<li><a href="contact.php">Contact</a></li>
+					<?php } ?>
 				</ul>
 			</nav>
 		</div>
