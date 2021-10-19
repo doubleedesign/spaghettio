ace0d5cb0c940efa4c2245b7bd414041d57d95fa
Leesa Ward <leesa.ward2@rmit.edu.au>
2021-10-19 16:47:03 +1100

    Update db and images

--- sql/restaurants.sql
+++ sql/restaurants.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 06, 2021 at 06:28 AM
+-- Generation Time: Oct 19, 2021 at 05:46 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.4.9
 
@@ -33,22 +33,24 @@ CREATE TABLE IF NOT EXISTS `restaurant_details` (
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `description` text NOT NULL,
+  `category` varchar(50) NOT NULL,
   `imagePath` varchar(255) NOT NULL,
   `imageCreator` varchar(255) NOT NULL,
   `imageSourceURL` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `restaurant_details`
 --
 
-INSERT INTO `restaurant_details` (`ID`, `name`, `address`, `description`, `imagePath`, `imageCreator`, `imageSourceURL`) VALUES
-(3, 'Ristorante Tartufo', '1000 Ann St, Fortitude Valley QLD 4006', 'Refined Neapolitan-inspired classics with seasonal produce in a smart red and black dining room.', 'uploads/calum-lewis-rPkgYDh2bmo-unsplash.jpg', 'Calum Lewis', 'https://unsplash.com/photos/rPkgYDh2bmo'),
-(6, 'Fasta Pasta', 'Cnr South Terrace Street &, Pulteney St, Adelaide SA 5000', 'Traditional pizza, pasta and Italian mains presented in an enduring chain with an easygoing vibe.', 'uploads/tom-dillon-9eIbwtyl4Xs-unsplash.jpg', 'Tom Dillon', 'https://unsplash.com/photos/9eIbwtyl4Xs'),
-(8, 'Il Gambero', '166 Lygon St, Carlton VIC 3053', 'Thin-crust pizza, plus pasta and mains, in a sleek venue with exposed-brick walls and outdoor seats.', 'uploads/inna-podolska-JspLKUauwSI-unsplash.jpg', 'Inna Podolska', 'https://unsplash.com/photos/JspLKUauwSI'),
-(17, 'The Spaghetti Tree', '59 Bourke St, Melbourne VIC 3000', 'The Spaghetti Tree has a welcoming and comfortable environment for your dining pleasure, and is a special place to visit for a walk down memory lane with pictures and photos of movie stars and entertainers from the Classical Hollywood era.', 'uploads/bruna-branco-t8hTmte4O_g-unsplash.jpg', 'Bruna Branco', 'https://unsplash.com/photos/t8hTmte4O_g'),
-(21, 'Foglia Di Fico', '585 La Trobe St, Melbourne VIC 3000', 'Polished, warm restaurant featuring Italian cuisine & handmade pasta, plus a robust wine list.', 'uploads/karthik-garikapati-oBbTc1VoT-0-unsplash (1).jpg', 'Karthik Garikapati', 'https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText');
+INSERT INTO `restaurant_details` (`ID`, `name`, `address`, `description`, `category`, `imagePath`, `imageCreator`, `imageSourceURL`) VALUES
+(3, 'Ristorante Tartufo', '1000 Ann St, Fortitude Valley QLD 4006', '1000 Ann St, Fortitude Valley QLD 4006', '', 'uploads/nerfee-mirandilla-o1EDsUFmuXQ-unsplash.jpg', 'Calum Lewis', 'https://unsplash.com/photos/rPkgYDh2bmo'),
+(6, 'Fasta Pasta', 'Cnr South Terrace Street &, Pulteney St, Adelaide SA 5000', 'Cnr South Terrace Street &, Pulteney St, Adelaide SA 5000', 'fast-food', 'uploads/karthik-garikapati-oBbTc1VoT-0-unsplash.jpg', 'Tom Dillon', 'https://unsplash.com/photos/9eIbwtyl4Xs'),
+(8, 'Il Gambero Test', '166 Lygon St, Carlton VIC 3053', '166 Lygon St, Carlton VIC 3053', 'casual', 'uploads/calum-lewis-rPkgYDh2bmo-unsplash.jpg', 'Inna Podolska', 'https://unsplash.com/photos/JspLKUauwSI'),
+(17, 'The Spaghetti Tree', '59 Bourke St, Melbourne VIC 3000', '59 Bourke St, Melbourne VIC 3000', '', 'uploads/clark-douglas-17ZU9BPy_Q4-unsplash.jpg', 'Bruna Branco', 'https://unsplash.com/photos/t8hTmte4O_g'),
+(21, 'Foglia Di Fico', '585 La Trobe St, Melbourne VIC 3000', '585 La Trobe St, Melbourne VIC 3000', '', 'uploads/karthik-garikapati-oBbTc1VoT-0-unsplash (1).jpg', 'Karthik Garikapati', 'https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'),
+(25, 'test', 'test', 'test', '', 'uploads/karthik-garikapati-oBbTc1VoT-0-unsplash.jpg', 'test', 'test');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
